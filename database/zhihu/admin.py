b7126f7bed8c8799462652bd1c7f29aa9e46bba4
liuxukun2000 <liuxk2019@mail.sustech.edu.cn>
2023-03-01 09:43:45 +0800

    database vis

--- database/zhihu/admin.py
+++ database/zhihu/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+import zhihu
+
+
+@admin.register(zhihu.models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "area"]
+    search_fields = ["area"]
+
+@admin.register(zhihu.models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["id", "content", "answer_count"]
+    # search_fields = ["area"]
+
+@admin.register(zhihu.models.Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "comment_count"]
+    # search_fields = ["area"]
+
+@admin.register(zhihu.models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "content"]
+    # search_fields = ["area"]
